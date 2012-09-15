4069d11d36f7f547c10f2ad9d5904995c0e4d0fc
Raul Portales <raul@plattysoft.com>
2012-09-15 14:44:40 +0200

    default api empty implementation

--- vhackandroidclient/src/nl/appsrus/vhack2012/api/ApiFactory.java
+++ vhackandroidclient/src/nl/appsrus/vhack2012/api/ApiFactory.java
@@ -2,4 +2,16 @@ package nl.appsrus.vhack2012.api;
 
 public class ApiFactory {
 
+	private static AbcApi sInstance;
+
+	private ApiFactory() {
+		// Hiding constructor of utility class
+	}
+	
+	public static AbcApi getInstance () {
+		if (sInstance == null) {
+			sInstance = new AbcApiImpl();
+		}
+		return sInstance;
+	}
 }
