7a031b5fe0ed2b7fa24bb78905ac941c4f05f925
Raul Portales <raul@plattysoft.com>
2012-09-15 19:47:04 +0200

    improving button of HB and adding api call

--- vhackandroidclient/src/nl/appsrus/vhack2012/MainActivity.java
+++ vhackandroidclient/src/nl/appsrus/vhack2012/MainActivity.java
@@ -14,7 +14,9 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,7 +27,7 @@ import com.viewpagerindicator.LinePageIndicator;
 
 public class MainActivity extends SherlockFragmentActivity implements ApiListener {
 	
-	private static JSONArray mUsers;
+	private static JSONArray sUsers;
 
 	private class MainActivityAdapter extends FragmentPagerAdapter {
 
@@ -35,15 +37,15 @@ public class MainActivity extends SherlockFragmentActivity implements ApiListene
 
 		@Override
 		public int getCount() {
-			if (mUsers == null) {
+			if (sUsers == null) {
 				return 1;
 			}
-			return mUsers.length() + 1;
+			return sUsers.length() + 1;
 		}
 
 		@Override
 		public Fragment getItem(int position) {
-			if (mUsers!= null && position < mUsers.length()){
+			if (sUsers!= null && position < sUsers.length()){
 				return MainFragment.newInstance(position);
 			}
 			return new NoMoreBirthdaysFragment();
@@ -51,9 +53,10 @@ public class MainActivity extends SherlockFragmentActivity implements ApiListene
 		
 	}
 	
-	public static class MainFragment extends SherlockFragment {
+	public static class MainFragment extends SherlockFragment implements OnClickListener, ApiListener {
 
 		private static final String POSITION = "position";
+		private static final String ACTION = "alreadySentHappyBD";
 
 		public static MainFragment newInstance(int position) {
 			MainFragment mainFragment = new MainFragment();
@@ -62,6 +65,9 @@ public class MainActivity extends SherlockFragmentActivity implements ApiListene
 			mainFragment.setArguments(args);
 			return mainFragment;
 		}
+
+		private int mUserId;
+		private JSONObject mUser;
 
 		@Override
 		public View onCreateView (LayoutInflater inflater, ViewGroup container,
@@ -74,27 +80,65 @@ public class MainActivity extends SherlockFragmentActivity implements ApiListene
 		@Override 
 		public void onViewCreated (View view, Bundle savedInstanceState) {
 			super.onViewCreated(view, savedInstanceState);
-			JSONObject user;
+			view.findViewById(R.id.sayHappyBirthday).setOnClickListener(this);
 			try {
-				user = mUsers.getJSONObject(getArguments().getInt(POSITION));
-				String firstName = user.getString("firstName");
+				mUser = sUsers.getJSONObject(getArguments().getInt(POSITION));
+				String firstName = mUser.getString("firstName");
+				mUserId = mUser.getInt("userId");
 				
 				String usesText = String.format(getString(R.string.usesPhoneFormat), firstName.toUpperCase());
-				
 				setText(R.id.firstName, firstName);
 				setText(R.id.usesPhone, usesText);
-				setText(R.id.lastName, user.getString("lastName"));
-				setText(R.id.tagline, user.getString("tagline"));
-				setText(R.id.phoneName, user.getString("phoneModel"));
-				setText(R.id.osVersion, user.getString("osVersion"));
+				setText(R.id.lastName, mUser.getString("lastName"));
+				setText(R.id.tagline, mUser.getString("tagline"));
+				setText(R.id.phoneName, mUser.getString("phoneModel"));
+				setText(R.id.osVersion, mUser.getString("osVersion"));
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			displayProperMessage();
 		}
 		
 		private void setText(int resId, String text) {
 			((TextView)getView().findViewById(resId)).setText(text);
+		}
+
+		@Override
+		public void onClick(View v) {
+			ApiFactory.getInstance().sayHappyBirthday(mUserId, this);
+		}
+
+		@Override
+		public void onError(int errorCode, String errorMessage) {
+			Toast.makeText(getActivity(), errorMessage, Toast.LENGTH_LONG).show();
+		}
+
+		@Override
+		public void onSuccess(JSONObject response) {
+			try {
+				// Mark it as done
+				mUser.put(ACTION, true);
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			displayProperMessage();
+		}
+
+		private void displayProperMessage() {
+			// Hide the button / Do an animation
+			Button button = (Button) getView().findViewById(R.id.sayHappyBirthday);
+			try {
+				if (mUser.getBoolean(ACTION)) {
+					button.setText(R.string.alreadyCongratulated);
+					button.setEnabled(false);
+					return;
+				}
+			} catch (JSONException e) {
+				// The attribute is not there, so we should show the say HB button
+			}
+			button.setText(R.string.sayHappyBirthday);
+			button.setEnabled(true);
 		}
 	}
 
@@ -146,7 +190,7 @@ public class MainActivity extends SherlockFragmentActivity implements ApiListene
 	@Override
 	public void onSuccess(JSONObject response) {
 		try {
-			mUsers = response.getJSONArray("users");
+			sUsers = response.getJSONArray("users");
 			mAdapter.notifyDataSetChanged();
 			mViewPager.invalidate();
 			mViewPager.setCurrentItem(0);
