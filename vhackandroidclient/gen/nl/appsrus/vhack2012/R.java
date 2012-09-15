bf560571fbef896bf2913a3849a2b87d81f3516c
Ronald van der Lingen <ronald@vdlingen.nl>
2012-09-15 18:17:22 +0200

    Changed view pager style and have placeholder layout for profile view

--- vhackandroidclient/gen/nl/appsrus/vhack2012/R.java
+++ vhackandroidclient/gen/nl/appsrus/vhack2012/R.java
@@ -1213,8 +1213,9 @@ containing a value of this type.
         public static final int abs__screen_simple_overlay_action_mode=0x7f030013;
         public static final int activity_main=0x7f030014;
         public static final int activity_profile=0x7f030015;
-        public static final int sherlock_spinner_dropdown_item=0x7f030016;
-        public static final int sherlock_spinner_item=0x7f030017;
+        public static final int fragment_profile_edit=0x7f030016;
+        public static final int sherlock_spinner_dropdown_item=0x7f030017;
+        public static final int sherlock_spinner_item=0x7f030018;
     }
     public static final class menu {
         public static final int activity_main=0x7f0b0000;
