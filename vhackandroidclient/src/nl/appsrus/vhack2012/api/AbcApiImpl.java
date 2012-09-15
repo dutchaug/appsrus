0a1134d351e4e1b540d21512234a8c780b137481
Raul Portales <raul@plattysoft.com>
2012-09-15 14:42:17 +0200

    default api empty implementation

---
+++ vhackandroidclient/src/nl/appsrus/vhack2012/api/AbcApiImpl.java
@@ -0,0 +1,27 @@
+package nl.appsrus.vhack2012.api;
+
+public class AbcApiImpl implements AbcApi {
+
+	public void getKey(String email, ApiListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void getBirthdays(ApiListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void sayHappyBirthday(int toUserId, ApiListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void updateUserProfile(String firstName, String lastName,
+			String c2dm, String tagline, int day, int month, int year,
+			ApiListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
