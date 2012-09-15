9124527c9a0891d8b54bf6d7e541858cd7c96b10
Raul Portales <raul@plattysoft.com>
2012-09-15 20:50:52 +0200

    removing already congratulated people from the result

--- vhackandroidServer/getBirthdays.php
+++ vhackandroidServer/getBirthdays.php
@@ -3,7 +3,21 @@ include 'common.php';
 
 $link = connectDB();
 
-$sql = "SELECT * FROM users WHERE day = DAY(CURDATE()) AND month = MONTH(CURDATE())";
+$authKey = $_POST["authKey"];
+
+if (isset ($authKey)) {
+	$sql = "SELECT * FROM users WHERE authToken = '".mysql_escape_string($authKey)."'";
+	$user = queryDb($link, $sql);
+	if (count($user) == 0){
+		dieWithError("Error authenticating", "401");
+	}
+	$userId = $user[0]["userId"];
+	$extra = " AND userId NOT IN (
+		SELECT toUserId FROM congrats 
+		WHERE fromUserId = '$userId' AND year = YEAR(CURDATE()))";
+}
+
+$sql = "SELECT * FROM users WHERE day = DAY(CURDATE()) AND month = MONTH(CURDATE()) $extra";
 $users = queryDb($link, $sql);
 
 $result = "";
